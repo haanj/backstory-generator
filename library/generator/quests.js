7ed0bf163e65a2c514892ba47780fdc1c75a8cbe
haanj <joshua.haan@ymail.com>
2016-06-13 12:26:48 -0700

    adds more quests

--- library/generator/quests.js
+++ library/generator/quests.js
@@ -4,7 +4,12 @@ module.exports = () => {
     [
       'Find who killed your brother',
       'Retrieve a rare artifact',
-      'Warn the king of impending doom'
+      'Warn the king of impending doom',
+      'Find the lost city',
+      'Drive the local lord from power',
+      'Save the farm',
+      'Start a rebellion',
+      'Explore the unexplored'
     ]
   let randomIndex = Math.floor(Math.random() * list.length)
   return list[randomIndex]
